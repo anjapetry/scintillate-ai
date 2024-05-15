9b1e6a4f1db77d68e94ca1572a161c120fc77f93
Anja Petry <anjapetry@protonmail.com>
2024-05-15 20:07:54 +0200

    update: modify Hero section

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,8 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+import ButtonLink from "@/components/ButtonLink";
 
 /**
  * Props for `Hero`.
@@ -12,10 +15,16 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
     <section
+      className="container"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for hero (variation: {slice.variation}) Slices
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <ButtonLink field={slice.primary.button_link}>
+        {slice.primary.button_label}
+      </ButtonLink>
+      <PrismicNextImage field={slice.primary.image} />
     </section>
   );
 };
