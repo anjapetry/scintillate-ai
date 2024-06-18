ce9af53a8d7d175fe33548b42b346c707b2372ed
Anja Petry <anjapetry@protonmail.com>
2024-06-18 18:50:07 +0200

    feat: add gsap animation to heading, body, button, image, glow in AnimatedContent.tsx

--- src/slices/Hero/AnimatedContent.tsx
+++ src/slices/Hero/AnimatedContent.tsx
@@ -19,7 +19,38 @@ export default function AnimatedContent({
 
   useGSAP(
     () => {
-      const tl = gsap.timeline();
+      const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+      tl.fromTo(
+        ".hero__heading",
+        { scale: 0.5 },
+        { scale: 1, opacity: 1, duration: 1.4 },
+      );
+
+      tl.fromTo(
+        ".hero__body",
+        { y: 20 },
+        { y: 0, opacity: 1, duration: 1.2 },
+        "-=0.6",
+      );
+
+      tl.fromTo(
+        ".hero__button",
+        { scale: 1.5 },
+        { scale: 1, opacity: 1, duration: 1.3 },
+        "-=0.8",
+      );
+      tl.fromTo(
+        ".hero__image",
+        { y: 100 },
+        { y: 0, opacity: 1, duration: 1.3 },
+        "+=0.3",
+      );
+      tl.fromTo(
+        ".hero__glow",
+        { scale: 0.5 },
+        { scale: 1, opacity: 1, duration: 1.8 },
+        "-=1",
+      );
     },
     { scope: container },
   );
@@ -28,13 +59,13 @@ export default function AnimatedContent({
     <div className="relative" ref={container}>
       <StarGrid />
       {isFilled.richText(slice.primary.heading) && (
-        <h1 className="hero__heading text-balance text-5xl font-medium md:text-7xl">
+        <h1 className="hero__heading text-balance text-5xl font-medium opacity-0 md:text-7xl">
           <PrismicText field={slice.primary.heading} />
         </h1>
       )}
 
       {isFilled.richText(slice.primary.body) && (
-        <div className="hero__body mx-auto mt-6 max-w-md text-balance text-slate-300">
+        <div className="hero__body mx-auto mt-6 max-w-md text-balance text-slate-300 opacity-0">
           <PrismicRichText field={slice.primary.body} />
         </div>
       )}
@@ -49,8 +80,8 @@ export default function AnimatedContent({
       )}
 
       {isFilled.image(slice.primary.image) && (
-        <div className="hero__image glass-container mt-16 w-fit">
-          <div className="hero__glow absolute inset-0 -z-10 bg-blue-300/30 blur-2xl filter" />
+        <div className="hero__image glass-container mt-16 w-fit opacity-0">
+          <div className="hero__glow absolute inset-0 -z-10 bg-blue-300/30 opacity-0 blur-2xl filter" />
           <PrismicNextImage
             className="rounded-lg"
             field={slice.primary.image}
