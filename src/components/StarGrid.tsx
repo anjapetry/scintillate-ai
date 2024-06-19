391f223d67ac70d810b231ea633df2f65a33a052
Anja Petry <anjapetry@protonmail.com>
2024-06-19 19:24:48 +0200

    feat: finish StarGrid.tsx animation including usePrefersReducedMotion

--- src/components/StarGrid.tsx
+++ src/components/StarGrid.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { Duru_Sans } from "next/font/google";
 
 export default function StarGrid() {
   const container = useRef(null);
@@ -12,20 +13,97 @@ export default function StarGrid() {
 
   const grid = [14, 30] as const;
 
-  useGSAP(() => {
-    gsap.set("#star-grid-item", {
-      opacity: 0,
-      transformOrigin: "center",
-      color: "#fff",
-    });
+  useGSAP(
+    () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { opacity: 1 });
+        gsap.set(".star-grid-item", {
+          opacity: 0.2,
+          scale: 1,
+        });
+        return;
+      }
 
-    const tl = gsap.timeline()
+      gsap.set(".star-grid-item", {
+        opacity: 0,
+        transformOrigin: "center",
+        color: "#fff",
+      });
+      gsap.set(container.current, { opacity: 1 });
 
-    // Entrance animation
+      const tl = gsap.timeline();
 
-    // Loop animation
+      // Entrance animation
+      tl.to(".star-grid-item", {
+        keyframes: [
+          {
+            opacity: 0,
+            duration: 0,
+          },
+          {
+            opacity: 0.4,
+            rotate: "+=180",
+            color: "#ffd057",
+            scale: 3,
+            duration: 0.6,
+            stagger: {
+              amount: 2,
+              grid: grid,
+              from: "center",
+            },
+          },
+          {
+            opacity: 0.2,
+            rotate: "+=180",
+            color: "#fff",
+            scale: 1,
+            delay: -2,
+            duration: 0.6,
+            stagger: {
+              amount: 3,
+              grid: grid,
+              from: "center",
+            },
+          },
+        ],
+      });
 
-  }, { scope: container });
+      // Loop animation
+      tl.to(".star-grid-item", {
+        delay: 8,
+        repeat: -1,
+        repeatDelay: 8,
+        keyframes: [
+          {
+            opacity: 0.4,
+            rotate: "+=180",
+            color: "#ffd057",
+            scale: 3,
+            duration: 0.6,
+            stagger: {
+              amount: 2,
+              grid: grid,
+              from: "center",
+            },
+          },
+          {
+            opacity: 0.2,
+            rotate: "+=180",
+            color: "#fff",
+            scale: 1,
+            delay: -2,
+            duration: 0.6,
+            stagger: {
+              amount: 3,
+              grid: grid,
+              from: "center",
+            },
+          },
+        ],
+      });
+    },
+    { scope: container },
+  );
 
   return (
     <svg
