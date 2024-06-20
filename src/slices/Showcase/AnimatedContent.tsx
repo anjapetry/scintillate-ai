3e69b6e051992f8757cd6f6395b59f22a64efb9c
Anja Petry <anjapetry@protonmail.com>
2024-06-20 14:40:14 +0200

    feat: implement gsap scrolltrigger animation on Showcase slice heading

--- src/slices/Showcase/AnimatedContent.tsx
+++ src/slices/Showcase/AnimatedContent.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+"use client";
 
-export default function AnimatedContent() {
-  return (
-    <div>AnimatedContent</div>
-  )
+import React from "react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function AnimatedContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const container = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(() => {
+    if (prefersReducedMotion) {
+      gsap.set(container.current, { y: 0 });
+      return;
+    }
+
+    gsap.fromTo(
+      container.current,
+      { y: 100 },
+      {
+        y: 0,
+        duration: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top bottom -=40%",
+          toggleActions: "play pause resume reverse",
+          // markers: true,
+        },
+      },
+    );
+  });
+
+  return <div ref={container}>{children}</div>;
 }
